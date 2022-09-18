16ee422fc9ee8e1b1c61d12a6eda6b1bc7192a05
Parveen Kumar <49730713+parveenmittal1@users.noreply.github.com>
2022-09-18 10:55:29 -0500

    simple select

---
+++ SQL/imtiaz ali udemi course/Select_statement.sql
@@ -0,0 +1,22 @@
+-- 1
+select * from employees;
+
+-- 2
+select department,email,first_name,last_name from employees;
+
+-- 3
+select * 
+from employees
+where department= 'Sports';
+
+-- 4 
+
+select * 
+from employees
+where department like '%Fur%';
+
+
+--5
+
+
+
